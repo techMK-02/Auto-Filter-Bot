f54de0154ff070383a4594e6a01f10483f774faf
DR BOTz <ashishyadav6716@gmail.com>
2025-04-15 20:28:47 +0530

    New Update ⚙️

--- plugins/route.py
+++ plugins/route.py
@@ -2,7 +2,6 @@ from aiohttp import web
 
 routes = web.RouteTableDef()
 
-
-@routes.get("/", allow_head=True)
+@routes.get("/", allow_head=True)    # Yeh health check route define kar rahe hain
 async def root_route_handler(request):
-    return web.json_response("Elsa")
+    return web.json_response("✅ Bot is alive!")    # Koyeb ko bata rahe hain ki bot alive hai
